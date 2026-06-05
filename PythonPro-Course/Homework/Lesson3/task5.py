0c5e0286a6b7e6eb30a2fcb4dc2f4d4bae922a15
Piotr Pietruliński <pietrulinski.01@o2.pl>
2026-06-05 14:45:56 +0200

    Delete PythonPro-Course/Homework/Lesson3 directory

--- PythonPro-Course/Homework/Lesson3/task5.py
+++
@@ -1,7 +0,0 @@
-# NazwaUzytkownika="JanKowalski"
-# wiekUzytkownika=25
-# if wiekUzytkownika>=18:print(NazwaUzytkownika+' jest dorosły.')
-Nazwa_Użytkownika = "Jan Kowalski"
-wiek_użytkownika = 25
-if wiek_użytkownika >= 18:
-    print(f'{Nazwa_Użytkownika} jest dorosły.') 
--- PythonPro-Course/Homework/Lesson3/task6.py
+++
@@ -1,8 +0,0 @@
-owoce = ["jabłko", "banan", "wiśnia"]
-print(owoce)
-owoce.append("pomarancza")
-print(owoce)
-owoce[1] = "jagoda"
-print(owoce)
-owoce.remove("jabłko")
-print(owoce)
