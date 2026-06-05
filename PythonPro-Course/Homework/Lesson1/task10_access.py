d381f66fe1389c53e34dac111560d584e0f06295
Piotr Pietruliński <pietrulinski.01@o2.pl>
2026-06-05 14:44:16 +0200

    Delete PythonPro-Course/Homework/Lesson1/task10_access.py

--- PythonPro-Course/Homework/Lesson1/task10_access.py
+++
@@ -1,8 +0,0 @@
-wzrost = float(input('Podaj wzrost: '))
-opiekun = input('Czy jest opiekun: ')
-if wzrost < 120:
-    print('Nie wchodzisz!')
-elif (120 <= wzrost < 160 and opiekun == 'tak') or (wzrost >= 160):
-    print('Wchodzisz.')
-else:
-    print('Nie Wchodzisz')
